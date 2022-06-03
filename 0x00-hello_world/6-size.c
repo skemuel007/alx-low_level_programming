43bbcf6e828496869f8e9b69e45808c6bbd31823
Salvation Kemuel <skemuel007@gmail.com>
2022-06-02 19:41:55 -0700

    Updated to fix error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,10 @@ int main(void)
 	/* Prints the sizes of various types */
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
-	printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(double));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
 	printf("Size of a long long int:");
 	printf(" %lu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 
 	return (0);
 }
