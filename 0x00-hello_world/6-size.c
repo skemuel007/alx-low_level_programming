c14f38c3d1272be766f7b6cdfa23b8dfa4cc12e9
Salvation Kemuel <skemuel007@gmail.com>
2022-06-02 19:37:44 -0700

    Fixed missing semi colon

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("Size of a double: %lu byte(s)\n", (unsigned long)sizeof(double));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int:")
+	printf("Size of a long long int:");
 	printf(" %lu byte(s)\n", (unsigned long)sizeof(long long int));
 
 	return (0);
