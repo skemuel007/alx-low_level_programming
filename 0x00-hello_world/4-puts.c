1d58ceae11a963286b8116bb7d0bf821bc1e8704
Salvation Kemuel <skemuel007@gmail.com>
2022-06-02 19:03:50 -0700

    Puts program complted

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Author: Salvation Lloyd Stanley-Kemuel
+ * Description: ALX puts test
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
