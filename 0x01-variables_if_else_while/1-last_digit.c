4d02dad6775689750d98949055e99a7b907c90e7
Salvation Kemuel <skemuel007@gmail.com>
2022-06-03 18:21:04 -0700

    Last digit committed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,18 +10,18 @@
 
 int main(void)
 {
-	int n;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int digit = n % 10;
+	digit = n % 10;
 
 	if (digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	else if (digit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, digit);
-	else if (lastDigit < 6 && lastDigit != 0)
+	else if (digit < 6 && digit != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 
 	return (0);
