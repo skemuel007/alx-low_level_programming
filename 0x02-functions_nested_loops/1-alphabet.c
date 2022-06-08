97e2b29cf84caae213307820214519531216e1ec
Salvation Kemuel <skemuel007@gmail.com>
2022-06-07 18:26:59 -0700

    Print alphabet a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Name of function
+ * Description: Prints alphabets in small letters
+ * Return: 0 if the program runs successfully
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+}
