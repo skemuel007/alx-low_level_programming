29a42aec8423186f387c1a9ca149f485b109f3a1
Salvation Kemuel <skemuel007@gmail.com>
2022-06-07 20:22:59 -0700

    Betty style for print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - A function to print sign of a number
+ * @n: takes an input type of integer n
+ * Description: Printing sign of a number
+ * Return: 1 is printed if program runs succesfully
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43 + 0);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
+		return (0);
+	}
+}
