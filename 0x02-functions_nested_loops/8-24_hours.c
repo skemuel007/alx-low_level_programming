83294180ecf98643fe0b4843724a8f9725cb8019
Salvation Kemuel <skemuel007@gmail.com>
2022-06-07 21:01:37 -0700

    Updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int mins, hours;
 
-	for (hours = 0; hours < 23; hours++)
+	for (hours = 0; hours <= 23; hours++)
 	{
-		for (mins = 0; mins < 59; mins++)
+		for (mins = 0; mins <= 59; mins++)
 		{
 			_putchar((hours / 10) + 48);
 			_putchar((hours % 10) + 48);
