1def44b9fa2ed7833f190c866ab630f089b1a34e
Salvation Kemuel <skemuel007@gmail.com>
2022-06-09 15:30:50 -0700

    Leap year and largest number updated

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-/*
+/**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ * Description: returns the largest number of 3 integers
  * Return: largest number
  */
 
