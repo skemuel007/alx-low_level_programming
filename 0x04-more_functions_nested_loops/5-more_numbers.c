a92b69f34318a822e7319c09806f05e435cf79cc
Salvation Kemuel <skemuel007@gmail.com>
2022-06-10 12:36:04 -0700

    Updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,21 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 11; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
+			k = j;
+
 			if (j > 9)
 			{
-				_putchar((j / 10) + '0');
+				_putchar(1 + 48);
+				k = j % 10;
 			}
 
-			_putchar((j % 10) + '0');
+			_putchar(k + 48);
 		}
 
 		_putchar('\n');
