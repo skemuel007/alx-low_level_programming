e0f3eabaa4b0ba5d007f8b63c94cc2f441e9419e
Salvation Kemuel <skemuel007@gmail.com>
2022-06-10 03:28:31 -0700

    Updated

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,16 @@
 
 /**
  * mul - Function name
- * @a - argument of type int as first param
- * @b - argument of type int as second param
+ * @a: argument of type int as first param
+ * @b: argument of type int as second param
  * Description: Multiplies two integers
  * Return: Returns multiple of two integers
  */
 int mul(int a, int b)
 {
-	return a * b;
+	int c;
+
+	c = a * b;
+
+	return(c);
 }
