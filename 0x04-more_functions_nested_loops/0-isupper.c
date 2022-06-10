346a7760b0838d2ba349ed600674e9d5dd0f540e
Salvation Kemuel <skemuel007@gmail.com>
2022-06-10 01:24:18 -0700

    Header file updated with isupper implemented

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Name of function
+ * @c: takes an input of character c
+ * Description: Checks if a character is upper
+ * Return: returns 1 if character is uppercase otherwise 0
+ */
+int _isupper(c)
+{
+	if ( ch >= 65 && ch <= 90)
+	{
+		return (1)
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# Working on More functions and Nested Loops
+<p>Validating my skills on C Nested Loops</p>
