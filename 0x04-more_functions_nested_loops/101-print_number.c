1459c18688246b3a14a717dc8d65da6e0a455941
Salvation Kemuel <skemuel007@gmail.com>
2022-06-10 18:06:35 -0700

    Print 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Function name
+ * @n: takes an input of n integer
+ * Description: Writes integer to output
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int m = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -m
+	}	
+
+	if ((m / 10 ) > 0)
+		print_number(m / 10);
+
+	_putchar((m % 10) + 48);
+}
