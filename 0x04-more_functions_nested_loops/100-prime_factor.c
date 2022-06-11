5f1738b73df8d52ae0e713bc13ceefaf77eb7b8f
Salvation Kemuel <skemuel007@gmail.com>
2022-06-10 18:17:43 -0700

    Updated

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,54 +1,5 @@
 #include <stdio.h>
-
-/**
- * square_root - Function name
- * @value: input type of double for argument
- * Description: Finds the square root
- * Return: retuns square root of the value in double
- */
-double square_root(double value)
-{
-	float sqrt, tmp;
-
-	sqrt = x / 2;
-	tmp = 0;
-
-	while (sqrt != tmp)
-	{
-		tmp = sqrt;
-		sqrt = (x / tmp + tmp)  / 2;
-	}
-	return (sqrt);
-}
-
-/**
- * max_prime_factor - Function name
- * @num: input type of long int
- * Description: Finds max prime
- * Return: void
- */
-void max_prime_factor(long int num)
-{
-	int prime_number, max_number;
-
-	while (num % 2 == 0)
-	{
-		num = num / 2;
-	}
-
-	for (prime_number = 3; prime_number <= square_root(num); prime_number += 2)
-	{
-		while (num % prime_number == 0)
-		{
-			num = num / prime_number;
-			max_number = prime_number;
-		}
-	}
-
-	if (num > 2)
-		max_number = num;
-	printf("%d\n", max_number);
-}
+#include <math.h>
 
 /**
  * main - Function name
@@ -57,7 +8,19 @@ void max_prime_factor(long int num)
  */
 int main(void)
 {
-	max_prime_factor(612852475143);
+	long a, maxv;
+	long number = 612852475143;
+	double square = sqrt(number);
+	
+	for (a = 1; a <= square; a++)
+	{
+		if (number % a == 0)
+		{
+			maxv = number / a;
+		}
+	}
+	
+	printf("%ld\n", maxv);
 
 	return (0);
 }
