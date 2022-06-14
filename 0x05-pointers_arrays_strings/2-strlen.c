830fd6c147014323515cde7cb03241206b5e08f1
Salvation Kemuel <skemuel007@gmail.com>
2022-06-13 20:50:51 -0700

    Str lenght updated

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Function name
+ * @s: argument of type char
+ * Description: Gets the length of a character
+ * Return: Int, count of the character
+ */
+int _strlen(char *s)
+{
+	char b = *s;
+
+	return (strlen(b));
+}
