364c406a3f0832de517615e7f4dd916538e7b062
Salvation Kemuel <skemuel007@gmail.com>
2022-06-13 21:21:51 -0700

    Updated

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,8 +10,8 @@ void print_rev(char *s)
 {
 	int count;
 
-	for (count = 0; *s != '\0'; ++s)
-		++count;
+	for (count = 0; s[count] != '\0'; ++count)
+		;
 
 	for (--count; count >= 0; --count)
 		_putchar(s[count]);
