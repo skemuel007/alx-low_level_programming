be9acbcbdb620751cc49d5504e11385a346aff5a
Salvation Kemuel <skemuel007@gmail.com>
2022-06-13 21:17:18 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_rev - Function name
+ *
+ * @s: string parameter input
+ * Return: void
+*/
+void print_rev(char *s)
+{
+	int count;
+
+	for (count = 0; s != '\0'; ++s)
+		++count;
+
+	for (--count; count >= 0; --count)
+		_putchar(s[count]);
+	_putchar('\n');
+}
