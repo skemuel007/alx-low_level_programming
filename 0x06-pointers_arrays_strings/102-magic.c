5926912ccf8bb8f7af0131d47e55dab3e698c3ab
Salvation Kemuel <skemuel007@gmail.com>
2022-06-17 16:26:05 -0700

    Magic code

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Function name
+ * Return: 0 if programs runs successfully
+ */
+int main(void)
+{
+	int n, a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+	*(p + 5) = 98;
+
+	printf("a[2] + %d\n", a[2]);
+
+	return (0);
+}
