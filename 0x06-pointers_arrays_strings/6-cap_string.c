32d88cee5d288ab229cdbca92b12185ec76efbc2
Salvation Kemuel <skemuel007@gmail.com>
2022-06-16 21:42:44 -0700

    Task 6 done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - Function name
+ * @s: pointer of char args
+ * Description: Capitalizes the first letter of every string
+ * Return: Capitalized first letter of every word
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			if (i == 0)
+			{
+				s[i] = s[i] - 32;
+			}
+
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+					s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+					s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+					s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+					s[i - 1] == 124)
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
