9fac743b2e7660bf1c70edfcb09fc44800bbb62a
Salvation Kemuel <skemuel007@gmail.com>
2022-06-16 22:24:24 -0700

    Updated

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rot13 - Function name
+ * @s: takes a pointer input type of char string
+ * Description: Encodes a string using rot13
+ * Return: endoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e','f','g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p','q',
+		'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y','Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	
+	while (str[++i])
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13key[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
