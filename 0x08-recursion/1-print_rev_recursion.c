ecd6e9c41b5fac899028e65a57cfeb5ea4fff7e4
Salvation Kemuel <skemuel007@gmail.com>
2022-06-22 18:04:46 -0700

    Updated

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in revers
+ * @s: Takes an input type of char pointer
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+}
