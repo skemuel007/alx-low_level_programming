42b322bdc155d10769dcc790d9ba7a31e092ed29
Salvation Kemuel <skemuel007@gmail.com>
2022-06-28 20:07:19 -0700

    Updated a third time

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,17 +2,15 @@
 #define MAIN_H
 
 #include <stdlib.h>
-#include <stdio.h>
 
+void free_grid(int **grid, int height);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int **alloc_grid(int width, int height);
 int len(char *str);
-unsigned int nbr_spaces(char *s)i;
 int num_words(char *str);
 
 #endif /* MAIN_H */
